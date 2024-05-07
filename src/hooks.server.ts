a09bcf3f38a2011f25c2215b708f7bc4d44673f6
Jaime <jguitron@ashleyfurniture.com>
2024-05-07 14:21:53 -0400

    hooks.server file created

---
+++ src/hooks.server.ts
@@ -0,0 +1,18 @@
+import type { Handle } from "@sveltejs/kit";
+import { sequence } from "@sveltejs/kit/hooks";
+
+export const handle1: Handle = async ({ event, resolve }) => {
+  const { locals, cookies, isDataRequest, url } = event;
+
+  if (!isDataRequest && url.pathname.startsWith("/api")) {
+    const token = cookies.get("token");
+
+    locals.user = token ? { name: "Jaime", id: 1 } : undefined;
+  }
+
+  const response = await resolve(event);
+
+  return response;
+};
+
+export const handle = sequence(handle1);
--- src/routes/products/+page.server.ts
+++ src/routes/products/+page.server.ts
@@ -3,7 +3,7 @@ import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ fetch, depends }) => {
   const response = await fetch("/api/products");
-  depends('app:productsServerLoad');
+  depends("app:productsServerLoad");
   if (response.ok) {
     return {
       products: await response.json(),
